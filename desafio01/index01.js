abb488992c256f4a480948d452f02ab735ef0a92
caualoch9860 <gamerpleyergp1@gmail.com>
2025-09-17 19:10:49 -0300

    Implementa Abstract Factory e Builder

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,69 @@
-console.log("Hello, World! 001");
+// Classes de Produto
+class LightButton {
+    render() {
+      return "Botão branco criado";
+    }
+  }
+  class DarkButton {
+    render() {
+      return "Botão preto criado";
+    }
+  }
+  class LightWindow {
+    render() {
+      return "Janela clara aberta";
+    }
+  }
+  class DarkWindow {
+    render() {
+      return "Janela escura aberta";
+    }
+  }
+  
+  // Fábrica Abstrata
+  class UIFactory {
+    createButton() {
+      throw new Error("Método abstrato deve ser implementado");
+    }
+    createWindow() {
+      throw new Error("Método abstrato deve ser implementado");
+    }
+  }
+  
+  // Fábricas Concretas
+  class LightFactory extends UIFactory {
+    createButton() {
+      return new LightButton();
+    }
+    createWindow() {
+      return new LightWindow();
+    }
+  }
+  
+  class DarkFactory extends UIFactory {
+    createButton() {
+      return new DarkButton();
+    }
+    createWindow() {
+      return new DarkWindow();
+    }
+  }
+  
+  // Cliente
+  function main(factory) {
+    const button = factory.createButton();
+    const windowUI = factory.createWindow();
+  
+    console.log(button.render());
+    console.log(windowUI.render());
+  }
+  
+  // Uso da aplicação
+  const lightFactory = new LightFactory();
+  const darkFactory = new DarkFactory();
+  
+  console.log(">> Tema Claro <<");
+  main(lightFactory);
+  
+  console.log("\n>> Tema Escuro <<");
+  main(darkFactory);
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,84 @@
-console.log("Hello, World! 002");
+// Produto
+class Lanche {
+    constructor(builder) {
+      this.pao = builder.pao;
+      this.carne = builder.carne;
+      this.queijo = builder.queijo;
+      this.salada = builder.salada;
+      this.molho = builder.molho;
+    }
+  
+    show() {
+      console.log("Lanche:", {
+        pao: this.pao,
+        carne: this.carne,
+        queijo: this.queijo,
+        salada: this.salada,
+        molho: this.molho,
+      });
+    }
+  }
+  
+  // Builder
+  class LancheBuilder {
+    constructor() {
+      this.pao = false;
+      this.carne = false;
+      this.queijo = false;
+      this.salada = false;
+      this.molho = false;
+    }
+  
+    comPao() {
+      this.pao = true;
+      return this;
+    }
+  
+    comCarne() {
+      this.carne = true;
+      return this;
+    }
+  
+    comQueijo() {
+      this.queijo = true;
+      return this;
+    }
+  
+    comSalada() {
+      this.salada = true;
+      return this;
+    }
+  
+    comMolho() {
+      this.molho = true;
+      return this;
+    }
+  
+    build() {
+      return new Lanche(this);
+    }
+  }
+  
+  // Diretor - para montagens pré-definidas
+  class LancheDirector {
+    static lancheSimples() {
+      return new LancheBuilder().comPao().comCarne().build();
+    }
+  
+    static lancheCompleto() {
+      return new LancheBuilder()
+        .comPao()
+        .comCarne()
+        .comQueijo()
+        .comSalada()
+        .comMolho()
+        .build();
+    }
+  }
+  
+  // Uso
+  const lancheSimples = LancheDirector.lancheSimples();
+  const lancheCompleto = LancheDirector.lancheCompleto();
+  
+  lancheSimples.show();
+  lancheCompleto.show();
